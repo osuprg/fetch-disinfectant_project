969923438969affea7ea3ac63ff175c31804e584
Alan G. Sanchez <sanchala@oregonstate.edu>
2020-12-23 11:51:27 -0800

    Implemented waypoint_generator class from rob599 project

--- fetch_disinfectant_project_moveit_config/src/waypoint_generator.py
+++ fetch_disinfectant_project_moveit_config/src/waypoint_generator.py
@@ -14,119 +14,142 @@ from grid_based_sweep_coverage_path_planner import planning_animation,planning
 
 class Waypoint_generator:
     def __init__(self):
-        rospy.init_node('waypoint_generator')
-        self.best_fit_plane_sub = rospy.Subscriber('best_fit_plane', PolygonStamped, self.update_data)
-        self.gui_input_sub = rospy.Subscriber('gui_input', String, self.callback )
-        self.bounding_box_pub = rospy.Publisher('bounding_box',Marker, queue_size=10)
-        self.waypoints_pub = rospy.Publisher('waypoints', PoseArray, queue_size = 10)
-        self.waypoints_marker_pub = rospy.Publisher('waypoints_marker', Marker, queue_size = 10)
+        # Initialize Subscribers
+        self.M_inv_array_sub = rospy.Subscriber('data_array',numpy_msg(Floats), self.data_callback, queue_size=1)
 
-        #Initialize the "get_points" node, parameters, Subscriber, and publisher
+        # Initialize Publishers
+        self.waypoints_pub        = rospy.Publisher('waypoints'       , PoseArray        , queue_size=1)
+        self.waypoints_marker_pub = rospy.Publisher('waypoints_marker', Marker           , queue_size=1)
+        self.missed_data          = rospy.Publisher('missed_data', numpy_msg(Floats), queue_size=1)
+
+        # Setup header
         self.header = Header()
         self.header.frame_id = "/base_link"
         self.header.stamp = rospy.Time.now()
 
-        self.bbox = Marker()
-        self.bbox.header = self.header
+        # Intialize the inverse matrix
+        self.M_inv = None
 
-        self.point_list = Marker()
-        self.point_list.header = self.header
+        # Intialize the X and Y coordinates of the 2D data points
+        self.X = None
+        self.Y = None
 
-        self.solver = Plane_fitting()
-        self.data = None
+        # Set the waypoint resolution (distance between points)
+        self.resolution = .05
 
-    def update_data(self,msg):
-        self.data = msg
-
-    def callback(self,gui_input):
-        if gui_input.data == "0":
-            if self.data != None:
-                self.bounding_box()
-            elif self.data == None:
-                print('You need to select at least 3 points to generate waypoints or update one of the 3 interactive markers.')
-
-
-    def bounding_box(self):
-        data = self.data
-        bbox_marker = self.bbox
-        bbox_marker.type = Marker.LINE_STRIP
-        bbox_marker.action = Marker.ADD
-        bbox_marker.id = 1
-        bbox_marker.scale.x = .03
-        bbox_marker.scale.y = .03
-        bbox_marker.scale.z = .03
-        bbox_marker.color.a = 1
-        bbox_marker.color.r = 1
-        bbox_marker.color.g = 1
-        bbox_marker.color.b = 0
-        bbox_marker.pose.position.x = 0
-        bbox_marker.pose.position.y = 0
-        bbox_marker.pose.position.z = 0
-        bbox_marker.pose.orientation.w = 1.0
-        poly_points = []
-        bbox_marker.points = []
-        for i in range(len(data.polygon.points)):
-            poly_points.append([data.polygon.points[i].x,data.polygon.points[i].y])
-
-        poly_xy_limits = BoundingBox(poly_points)
-        bbox_2D = poly_xy_limits.to_polygon()
-        for j in range(len(bbox_2D)):
-            z_value = self.solver.solve_for_z(bbox_2D[j][0],bbox_2D[j][1])
-            bbox_marker.points.append(Point(bbox_2D[j][0],bbox_2D[j][1],z_value))
-            if j == (len(bbox_2D)-1):
-                z_value = self.solver.solve_for_z(bbox_2D[0][0],bbox_2D[0][1])
-                bbox_marker.points.append(Point(bbox_2D[0][0],bbox_2D[0][1],z_value))
-        self.bounding_box_pub.publish(bbox_marker)
-        self.generate_waypoints(poly_xy_limits,Polygon(poly_points))
-
-    def generate_waypoints(self,poly_xy_limits,poly_points):
+        # Initialize waypoints as a PoseArray
         self.waypoints = PoseArray()
         self.waypoints.header = self.header
-        dist_from_surface = 1
-        margin = .1
-        x_lim_min = poly_xy_limits.min_point[0] + margin
-        x_lim_max = poly_xy_limits.max_point[0] - margin
-        y_lim_min = poly_xy_limits.min_point[1] + margin
-        y_lim_max = poly_xy_limits.max_point[1] - margin
-        num_x = abs(round((x_lim_min-x_lim_max)/(margin)))
-        num_y = abs(round((y_lim_min-y_lim_max)/(margin)))
-        x = np.linspace(x_lim_min, x_lim_max,num = num_x)
-        y = np.linspace(y_lim_max, y_lim_min,num = num_y)
-        XX,YY = np.meshgrid(x,y)
-        row, col = XX.shape
-        list = []
-        for m in range(row):
-            for n in range(col):
-                if poly_points.contains(Point_2D(XX[m][n],YY[m][n])) == True:
-                    p = Pose()
-                    p.position.x = XX[m][n]
-                    p.position.y = YY[m][n]
-                    p.position.z = np.float64(self.solver.solve_for_z(XX[m][n], YY[m][n])+.4)
-                    list.append(Point(XX[m][n],YY[m][n],p.position.z))
-                    p.orientation.w = 1.0
-                    self.waypoints.poses.append(p)
 
+        # Initialize waypoint_markers and all of the other feature values
+        self.waypoints_marker = Marker()
+        self.waypoints_marker.header = self.header
+        self.waypoints_marker.type = Marker.LINE_STRIP
+        self.waypoints_marker.action = Marker.ADD
+        self.waypoints_marker.id = 1
+        self.waypoints_marker.scale.x = .01
+        self.waypoints_marker.scale.y = .01
+        self.waypoints_marker.color.a = 1
+        self.waypoints_marker.color.r = 0
+        self.waypoints_marker.color.g = 0
+        self.waypoints_marker.color.b = 1.0
+
+
+
+    def data_callback(self, data_msg):
+        # Splice the data: Inverse matrix and the 2D coordinates
+        M_inv_arr = data_msg.data[:16]
+        coord_2D_arr = data_msg.data[16:]
+        coord_arr_len = len(coord_2D_arr)
+
+        # Set Inverse transfer Matrix (M_inv) to equal an empty matrix.
+        self.M_inv = np.empty(shape=[4,4])
+
+        # assign X and Y to empty lists
+        self.X = []
+        self.Y = []
+
+        # Fill in the the inverse matrix with the subscribed data
+        for i in range(4):
+            self.M_inv[i][0] = M_inv_arr[i*4 + 0]
+            self.M_inv[i][1] = M_inv_arr[i*4 + 1]
+            self.M_inv[i][2] = M_inv_arr[i*4 + 2]
+            self.M_inv[i][3] = M_inv_arr[i*4 + 3]
+
+        # Append the X and Y values of the coordinates positions
+        for i in range(coord_arr_len):
+            if (i % 2) == 0:
+                self.X.append(coord_2D_arr[i])
+            else:
+                self.Y.append(coord_2D_arr[i])
+
+            # The planner needs a closed loop of points, hence why the first set
+            # of coordinates are appended at the end of the lists.
+            if (i+1) == coord_arr_len:
+                self.X.append(coord_2D_arr[0])
+                self.Y.append(coord_2D_arr[1])
+
+        # Run waypoint_planner function
+        self.waypoint_planner()
+
+
+
+    def waypoint_planner(self):
+        # Acquire the planned x an y values from the planning function
+        px,py = planning(self.X, self.Y, self.resolution)
+
+        # Create lists and array of waypoints to publish
+        poses = []
+        marker_list = []
+        pose_arr = np.empty(shape=[len(px),2])
+
+        # Begin dimension increase for 2D coordinates (px, py)
+        for i in range(len(px)):
+            arr_2D = np.array([px[i], py[i], 0, 1])
+            dim_incr = np.matmul(self.M_inv, arr_2D)
+            pose_arr[i] = [px[i], py[i]]
+
+            # poses append to poses (Pose Array)
+            p = Pose()
+            p.position.x = dim_incr[0]
+            p.position.y = dim_incr[1]
+            p.position.z = dim_incr[2]
+            p.orientation.x = 0.0
+            p.orientation.y = 0.0
+            p.orientation.z = 0.0
+            p.orientation.w = 1.0
+            poses.append(p)
+
+            # Append position values for the marker
+            marker_list.append(Point(p.position.x, p.position.y, p.position.z))
+
+        # assisn poses to the PoseArray, self,waypoints.
+        self.waypoints.poses = poses
+
+        # Publish poses for computeCartesianPath
         self.waypoints_pub.publish(self.waypoints)
-        self.waypoint_marker(list)
 
-    def waypoint_marker(self,list):
-        marker = self.point_list
-        poly_points_2D = []
-        triangulation_points = []
-        marker.type = Marker.POINTS
-        marker.action = Marker.ADD
-        marker.id = 1
-        marker.scale.x = .01
-        marker.scale.y = .01
-        marker.color.a = 1
-        marker.color.r = 0
-        marker.color.g = 0
-        marker.color.b = 1.0
-        marker.points = list
-        self.waypoints_marker_pub.publish(marker)
+        # Publish markers for waypoints
+        self.waypoints_marker.points = marker_list
+        self.waypoints_marker_pub.publish(self.waypoints_marker)
 
+        # Create an array for three missed points
+        missed_points = np.empty(shape=[4,2])
+
+        # For loop to randomly select points for the missed points array
+        for i in range(4):
+            e = random.randint(0,len(pose_arr)-1)
+            missed_points[i] = pose_arr[e]
+
+
+        # Publish the inverse matrix and missed points for optimization node
+        a = np.array(self.M_inv.ravel(), dtype=np.float32)
+        b = np.array(missed_points.ravel(), dtype=np.float32)
+        self.missed_data.publish(np.concatenate((a,b)))
 
 
 if __name__=="__main__":
+    # Initialize the node
+    rospy.init_node('waypoint_generator')
     Waypoint_generator()
     rospy.spin()
