12501630d50047391ec64dcb26613c122fbb3acf
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-01-26 23:31:22 -0800

    INlcuded decimial and fraction parameters in the SliderDisplay class

--- fetch_disinfectant_project_moveit_config/src/slider.py
+++ fetch_disinfectant_project_moveit_config/src/slider.py
@@ -9,13 +9,15 @@ from PyQt5.QtCore import Qt
 
 class SliderDisplay(QWidget):
 
-    def __init__(self, name, min, max, ticks, parent = None):
-        super(SliderDisplay, self).__init__(parent)
+    def __init__(self, name, min, max, ticks, dec = 0, fraction = 0):
+        super(SliderDisplay, self).__init__()
         self.name  = name
         self.min   = min
         self.max   = max
         self.ticks = ticks
-        self.val = 1.0/self.ticks
+        self.dec   = dec
+        self.fraction = fraction
+        self.val   = 1.0/self.ticks
 
 
         self.slider = QSlider(Qt.Horizontal)
@@ -34,20 +36,26 @@ class SliderDisplay(QWidget):
         self.show()
 
     def v_change(self):
-        self.val = float(self.slider.value())/self.ticks*self.max
-        sig = str(int(math.log10(self.ticks)))
-        string = '{0}: {1:.' + sig +'f}'
-        self.label.setText(string.format(self.name,self.val))
+        if self.fraction == 0:
+            self.val = float(self.slider.value())/self.ticks*self.max
+            sig = str(int(math.log10(self.ticks)))
+            string = '{0}: {1:.' + str(self.dec) +'f}'
+            self.label.setText(string.format(self.name,self.val))
+        else:
+            self.val = float(self.slider.value())/self.ticks*self.max/(10**self.fraction)
+            sig = str(int(math.log10(self.ticks)))
+            string = '{0}: {1:.' + str(self.dec) +'f}'
+            self.label.setText(string.format(self.name,self.val))
 
     def value(self):
         return self.val
-        #' ' + str(my_value))
+        
 
-def main_f(name, min, max, ticks):
+def main_f(name, min, max, ticks, dec = 0):
     app = QApplication(sys.argv)
-    ex = SliderDisplay(name, min, max, ticks)
+    ex = SliderDisplay(name, min, max, ticks, dec)
     ex.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    main_f('Try', 0, 1, 100)
+    main_f('Try', 0, 1, 100, 2)
