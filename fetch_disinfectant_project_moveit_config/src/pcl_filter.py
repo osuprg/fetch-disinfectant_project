2d5cef34c2bf993ad9a4c1293d269361012865f3
Alan G. Sanchez <sanchala@oregonstate.edu>
2022-01-17 15:38:50 -0800

    Include transformation function in pcl_filter node.

--- fetch_disinfectant_project_moveit_config/src/pcl_filter.py
+++ fetch_disinfectant_project_moveit_config/src/pcl_filter.py
@@ -1,85 +1,89 @@
 #!/usr/bin/env python
 
-
-
-"""
-
-"""
-
-
-
 # Import modules
 import rospy
-# import pcl
 import numpy as np
 import ctypes
 import struct
+import tf
 import sensor_msgs.point_cloud2 as pc2
 
-from sensor_msgs.msg import PointCloud2, PointField
+# Import message types and other python libraries
+from transform_frame_node import transformer
+from sensor_msgs.msg import PointCloud2, PointCloud
+from geometry_msgs.msg import Point32, PolygonStamped
 from std_msgs.msg import Header
-from random import randint
-
+from shapely import geometry
 
 class PointCloudFilter:
     def __init__(self):
-        #Initialize Pointcloud Subscriber
-        self.pointcloud2_sub = rospy.Subscriber("/realsense/depth/color/points",PointCloud2, self.ros_to_pcl, queue_size=1)
+        # Initialize Subscribers
+        self.pointcloud2_sub = rospy.Subscriber("/head_camera/depth_downsample/points"  , PointCloud2    ,self.pointcloud_data, queue_size=1)
+        self.plane_poly_sub  = rospy.Subscriber("/best_fit_plane_poly"                  , PolygonStamped ,self.pcl_filter, queue_size=1)
 
-        self.flag = 0
+        # Initialize PointCloud Publisher
+        self.pointcloud_pub = rospy.Publisher("/filtered_cloud", PointCloud, queue_size=1)
+
+        # Initialize filt_cloud as a PointCloud message type
+        self.filt_cloud = PointCloud()
+        self.filt_cloud.header = Header()
+        self.filt_cloud.header.stamp = rospy.Time.now()
+        self.filt_cloud.header.frame_id = 'base_link'
+
+        # Intialize pcl_data as an empty list
+        self.pcl_data = []
+
+        # Initialize self.cloud for data storage in pointcloud_data callback function
+        self.cloud = None
+
+    def pointcloud_data(self,ros_cloud):
+        # Store pointcloud2 data
+        self.cloud = ros_cloud
+
+    def pcl_filter(self, polygon):
+        # Run transformer function to get transform from the head_camera_depth_optical_frame
+        # to the base_link
+        trans,_ = self.transformer()
+        # For loop to extract ros_cloud data into a list of x,y,z, and RGB (float)
+        for data in pc2.read_points(self.cloud, skip_nans=True):
+            self.pcl_data.append([trans[0] + data[2], trans[1] - data[0], trans[2] - data[1]])
+
+        # Intialize region as a list.
+        region = []
+
+        # Run forloop to store polygon vertices in list named region.
+        for vertices in polygon.polygon.points:
+            region.append([vertices.x,vertices.y])
+
+        for points in self.pcl_data:
+            X = points[0]
+            Y = points[1]
+            line = geometry.LineString(region)
+            point = geometry.Point(X, Y)
+            polygon = geometry.Polygon(line)
+
+            if polygon.contains(point) == True:
+                self.filt_cloud.points.append(Point32(points[0],points[1],points[2]))
+
+        # Publish filtered point_cloud data
+        self.pointcloud_pub.publish(self.filt_cloud)
+
+        # Reset filtered data points.
+        del self.filt_cloud.points[:], self.pcl_data[:]
 
 
-    def ros_to_pcl(self, ros_cloud):
-        """ Converts a ROS PointCloud2 message to a pcl PointXYZRGB
+    def transformer(self):
+        listener = tf.TransformListener()
 
-            Args:
-                ros_cloud (PointCloud2): ROS PointCloud2 message
-
-            Returns:
-                pcl.PointCloud_PointXYZRGB: PCL XYZRGB point cloud
-        """
-        # print(ros_cloud)
-        # points_list = []
-        #
-
-        # pcl_data = pcl.PointCloud_PointXYZRGB()
-        # pcl_data.from_list(points_list)
-
-        if self.flag == 0:
-            print(ros_cloud)
-
-            for data in pc2.read_points(ros_cloud, skip_nans=True):
-                # points_list.append([data[0], data[1], data[2], data[3]])
-                print([data[0],data[1],data[2],data[3]])
-            self.flag = 1
-        # return pcl_data
-
-    def pointcloud2_to_array(cloud_msg, squeeze=True):
-        ''' Converts a rospy PointCloud2 message to a numpy recordarray
-
-        Reshapes the returned array to have shape (height, width), even if the height is 1.
-
-        The reason for using np.fromstring rather than struct.unpack is speed... especially
-        for large point clouds, this will be <much> faster.
-        '''
-        # construct a numpy record type equivalent to the point type of this cloud
-        dtype_list = fields_to_dtype(cloud_msg.fields, cloud_msg.point_step)
-
-        # parse the cloud into an array
-        cloud_arr = np.fromstring(cloud_msg.data, dtype_list)
-
-        # remove the dummy fields that were added
-        cloud_arr = cloud_arr[
-            [fname for fname, _type in dtype_list if not (fname[:len(DUMMY_FIELD_PREFIX)] == DUMMY_FIELD_PREFIX)]]
-
-        if self.flag == 0:
-
-            self.flag = 1
-            if squeeze and cloud_msg.height == 1:
-                print(np.reshape(cloud_arr, (cloud_msg.width,)))
-            else:
-                print(np.reshape(cloud_arr, (cloud_msg.height, cloud_msg.width)))
-
+        while not rospy.is_shutdown():
+            try:
+                (trans,rot) = listener.lookupTransform("/base_link" ,"/head_camera_depth_optical_frame",  rospy.Time(0))
+                return trans,rot
+                if trans:
+                    break
+        # This will give you the coordinate of the child in the parent frame
+            except (tf.LookupException, tf.ConnectivityException,tf.ExtrapolationException):
+                pass
 
 if __name__=="__main__":
     rospy.init_node('pcl_filter',anonymous=True)
