80d88c4a07d375c1fb81dab035c8af987a02209a
Alan G. Sanchez <sanchala@oregonstate.edu>
2022-01-21 16:03:13 -0800

    Include euler_to_quaternion function

--- fetch_disinfectant_project_moveit_config/src/nonplanar_waypoint_generator.py
+++ fetch_disinfectant_project_moveit_config/src/nonplanar_waypoint_generator.py
@@ -4,6 +4,7 @@
 import rospy
 import random
 import numpy as np
+import pyvista as pv
 
 from scipy import spatial
 from visualization_msgs.msg import Marker
@@ -17,9 +18,8 @@ from grid_based_sweep_coverage_path_planner import planning_animation,planning
 class Waypoint_generator:
     def __init__(self):
         # Initialize Subscribers
-        self.plane_poly_sub  = rospy.Subscriber("/best_fit_plane_poly"  , PolygonStamped ,self.polygon_callback, queue_size=1)
-        # self.IM_array_sub    = rospy.Subscriber('IM_pose_array' ,numpy_msg(Floats)  ,self.best_fit_plane_callback   ,queue_size=1)
-        self.pointcloud_sub  = rospy.Subscriber("filtered_cloud"          ,PointCloud         ,self.data_pointcloud           ,queue_size=1)
+        self.plane_poly_sub  = rospy.Subscriber("/best_fit_plane_poly"  ,PolygonStamped ,self.polygon_callback  ,queue_size=1)
+        self.pointcloud_sub  = rospy.Subscriber("filtered_cloud"        ,PointCloud     ,self.data_pointcloud   ,queue_size=1)
 
 
         # Initialize Publishers
@@ -49,11 +49,12 @@ class Waypoint_generator:
         # Initialize waypoint_markers and all of the other feature values
         self.waypoints_marker = Marker()
         self.waypoints_marker.header = self.header
-        self.waypoints_marker.type = Marker.LINE_STRIP
+        self.waypoints_marker.type = Marker.ARROW
         self.waypoints_marker.action = Marker.ADD
-        self.waypoints_marker.id = 1
-        self.waypoints_marker.scale.x = .01
-        self.waypoints_marker.scale.y = .01
+        # self.waypoints_marker.id = 0
+        self.waypoints_marker.scale.x = 0.04
+        self.waypoints_marker.scale.y = 0.03
+        self.waypoints_marker.scale.z = 0.01
         self.waypoints_marker.color.a = 1
         self.waypoints_marker.color.r = 0
         self.waypoints_marker.color.g = 0
@@ -68,21 +69,6 @@ class Waypoint_generator:
         self.offset = 0.3
 
 
-    def best_fit_plane_callback(self,arr_msg):
-        # Conditional statement to process arr_msg
-        if len(arr_msg.data) < 9:
-            pass
-
-        else:
-            self.X = []
-            self.Y = []
-            # Sepearately store the components (XYZ) of the IM's
-            for i in range(int(len(arr_msg.data)/3)):
-                # print(arr_msg.data[i*3 + 0])
-                self.X.append(arr_msg.data[i*3 + 0])
-                self.Y.append(arr_msg.data[i*3 + 1])
-
-
     def polygon_callback(self,polygon_msg):
         self.X = []
         self.Y = []
@@ -95,16 +81,12 @@ class Waypoint_generator:
         self.Y.append(self.Y[0])
 
 
-
-
-            # print(arr_msg.data)
     def data_pointcloud(self,pointcloud_msg):
         self.cloud_x = []
         self.cloud_y = []
         self.cloud_z = []
 
         for i in range(len(pointcloud_msg.points)):
-            # self.filtered_cloud.append([pointcloud_msg.points[i].x, pointcloud_msg.points[i].y, pointcloud_msg.points[i].z])
             self.cloud_x.append(pointcloud_msg.points[i].x)
             self.cloud_y.append(pointcloud_msg.points[i].y)
             self.cloud_z.append(pointcloud_msg.points[i].z)
@@ -122,39 +104,64 @@ class Waypoint_generator:
         tree = spatial.KDTree(np.c_[self.cloud_x,self.cloud_y])
 
 
+        # simply pass the numpy points to the PolyData constructor
+        cloud = pv.PolyData(np.c_[self.cloud_x, self.cloud_y, self.cloud_z])
+        surf = cloud.delaunay_2d()
+
+
 
         # Create lists and array of waypoints to publish
         poses = []
         marker_list = []
 
-        # Begin dimension increase for 2D coordinates (px, py) and store new coordinates
+        # run KD tree function on planned x and y points
         for i in range(len(px)):
-            # waypoint = np.matmul(self.M_inv, np.array([px[i], py[i], 0, 1]))
+            # Get index values of closest neighbors
+            closest_points_ii = tree.query_ball_point([px[i],py[i]], 0.075)
 
-            # poses append to poses (Pose Array)
-            closest_points_ii = tree.query_ball_point([px[i],py[i]], 0.05)
 
             if len(closest_points_ii ) == 0:
                 continue
 
-            z = []
+            z_val = []
+            # append the z values of the point cloud of the indexed values.
             for j in closest_points_ii:
-                z.append(self.cloud_z[j])
+                z_val.append(self.cloud_z[j])
+
+            # Find the index of the max z value
+            index = z_val.index(max(z_val))
+
+            # convert the point normal of the max z value to euler angles
+            euler_x = np.arccos(surf.point_normals[index][0])
+            euler_y = np.arccos(surf.point_normals[index][1])
+            euler_z = np.arccos(surf.point_normals[index][2])
+
+            # convert to Quaternion values
+            qx,qy,qz,qw = self.euler_to_quaternion(euler_x, euler_y , euler_z )
+            # if qx > 0 and qz < 0:
+            #     qx = -qx
+            #     qz = abs(qz)
 
 
+            # print(qx,qy,qz,qw)
+            # print(' ')
+            
             p = Pose()
             p.position.x = px[i]
             p.position.y = py[i]
-            p.position.z = max(z) + self.offset
-            p.orientation.x = 0.0
-            p.orientation.y = 0.0
-            p.orientation.z = 0.0
-            p.orientation.w = 1.0
+            p.position.z = max(z_val) + self.offset
+            p.orientation.x = qx
+            p.orientation.y = qy
+            p.orientation.z = qz
+            p.orientation.w = qw
             poses.append(p)
 
-            # Append position values for the marker
-            marker_list.append(Point(p.position.x, p.position.y, p.position.z))
 
+            # Append position values for the marker
+            # marker_list.append(Point(p.position.x, p.position.y, p.position.z))
+            self.waypoints_marker.id = i
+            self.waypoints_marker.pose = p
+            self.waypoints_marker_pub.publish(self.waypoints_marker)
 
 
         # assisn poses to the PoseArray, self,waypoints.
@@ -163,12 +170,19 @@ class Waypoint_generator:
         # Publish poses for computeCartesianPath
         self.waypoints_pub.publish(self.waypoints)
 
-        # Publish markers for waypoints
-        self.waypoints_marker.points = marker_list
-        self.waypoints_marker_pub.publish(self.waypoints_marker)
+        ## Publish markers for waypoints
+        # self.waypoints_marker.points = marker_list
+        # self.waypoints_marker_pub.publish(self.waypoints_marker)
 
         del self.cloud_x[:], self.cloud_y[:], self.cloud_z[:]
 
+    def euler_to_quaternion(self,yaw, pitch, roll):
+        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+        return [qx, qy, qz, qw]
 
 if __name__=="__main__":
     # Initialize the node
